c8c36640fb16adc4c1409c4a3738c08e89cb9fea
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-18 22:39:25 +0530

    extra code for inheritance type

--- src/main/java/com/demo/entity/Staff.java
+++ src/main/java/com/demo/entity/Staff.java
@@ -7,11 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@DiscriminatorColumn(name = "type")
+@DiscriminatorColumn(name = "type")//optional default table name is like dtype
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)//if we remove by default it will work like single table
 @Entity
 @Data @NoArgsConstructor
 public class Staff implements Serializable{
